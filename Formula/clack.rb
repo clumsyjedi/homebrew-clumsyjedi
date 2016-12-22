99cca424374bfebef803d61d04b835999aee3b93
Frazer Irving <frazer.irving@mailonline.com>
2016-12-22 12:56:01 +1100

    its a new sha yo

--- Formula/clack.rb
+++ Formula/clack.rb
@@ -2,7 +2,7 @@ class Clack < Formula
   desc "Sort of like jq or maybe ack but for Clojure"
   homepage "https://github.com/clumsyjedi/clack"
   url "https://github.com/clumsyjedi/clack/raw/master/bundle/clack-0.0.1.tar.gz"
-  sha256 "db4c80851b31d29fac564478e16a11cc4b805e5e50a4bbb9f319dbfdd6b8be39"
+  sha256 "2d06ba7f733cbf0a538886def830cc6c63f91fa65be46df10edccc3dcc0fb468"
 
   depends_on "coreutils" => :build
   depends_on "node" => :run
