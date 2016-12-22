338176dffc1e5a6966a7ee172f9376dba9b934df
Frazer Irving <frazer.irving@mailonline.com>
2016-12-22 13:58:34 +1100

    new version

--- Formula/clack.rb
+++ Formula/clack.rb
@@ -1,11 +1,11 @@
 class Clack < Formula
   desc "Sort of like jq or maybe ack but for Clojure"
   homepage "https://github.com/clumsyjedi/clack"
-  url "https://github.com/clumsyjedi/clack/raw/master/bundle/clack-0.1.1.tar.gz"
-  sha256 "422d5b7817d9623e10a99b9317a47e1233eb71050ae5349290d983a2ff676e5f"
+  url "https://github.com/clumsyjedi/clack/raw/master/bundle/clack-0.1.2.tar.gz"
+  sha256 "69d1e0bf77e6920f455d618788771b616f254c9b3b8b4d9917017cc3db5c8f4e"
 
-  depends_on "coreutils" => :build
-  depends_on "node" => :run
+  depends_on "coreutils" => :run
+  depends_on "node" => :optional
 
   def install
       system "cp -rp * #{prefix}"
